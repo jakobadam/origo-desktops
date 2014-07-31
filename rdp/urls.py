d8c6f85b4e325cdd0607b2db60c73732110185ee
Jakob A. Dam <jakob@cabo.dk>
2014-07-31 10:51:29 +0200

    Remove admin - which causes an url error?

--- rdp/urls.py
+++ rdp/urls.py
@@ -1,14 +1,11 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls import patterns, url
 
-from django.contrib import admin
-admin.autodiscover()
+# from django.contrib import admin
+# admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'ghostapp.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
+    # url(r'^admin/', include(admin.site.urls)),
 
-    url(r'^admin/', include(admin.site.urls)),
     url(r'^$', 'rdp.views.programs', name="programs"),
     url(r'^programs/add/$', 'rdp.views.add_program', name="add_program"),
     url(r'^programs/delete/$', 'rdp.views.delete_program', name="delete_program"),
@@ -19,4 +16,5 @@ urlpatterns = patterns('',
     url(r'^setup/domain$', 'rdp.views.domain_setup', name="domain_setup"),
     url(r'^setup/password$', 'rdp.views.password_setup', name="password_setup"),
 
+    url(r'^api/ip$', 'rdp.views.ip'),
 )
