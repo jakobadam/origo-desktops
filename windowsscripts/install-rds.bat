da2680c62c4a75139a5911b75c9abf38cf285a96
Jakob A. Dam <jakob@cabo.dk>
2014-08-07 09:11:13 +0200

    Clean up

--- windowsscripts/install-rds.bat
+++
@@ -1,2 +0,0 @@
-REM Magic variable %~dp0 gives dirname of script
-cmd.exe /c %~dp0rds-install-step1.bat
--- winexe/api.py
+++
@@ -1,7 +0,0 @@
-from .models import Request
-
-def run(method, **kwargs):
-    return Request(method, **kwargs).send()
-
-def cmd(**kwargs):
-    return Request('cmd', **kwargs).send()
