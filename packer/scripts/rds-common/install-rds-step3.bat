6438f0fb9a5a9ca5ef33982f97eb75ed0f9b86db
Jakob A. Dam <jakob@cabo.dk>
2014-08-21 16:19:03 +0200

    Install SSH as the last step

--- packer/scripts/rds-common/install-rds-step3.bat
+++ packer/scripts/rds-common/install-rds-step3.bat
@@ -1,11 +1,10 @@
 :: Deleting old scheduled task
 schtasks /delete /tn "Install RDS Step 3" -f
 
-:: Installing Cygwin
+:: Schedule callback to admin server
+cmd /c %~dp0ip-task.bat
 
+:: Installing Cygwin
 cmd /c %~dp0install-cygwin-sshd.bat
 cmd /c %~dp0vagrant-ssh.bat
 
-:: Schedule callback to admin server
-
-cmd /c %~dp0ip-task.bat
