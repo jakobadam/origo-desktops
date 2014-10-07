eb2e9719a73eaff5d9aaab12043f48bb277f603f
Jakob A. Dam <jakob@cabo.dk>
2014-10-07 11:56:52 +0200

    Install script updated

--- Vagrantfile
+++ Vagrantfile
@@ -18,7 +18,6 @@ Vagrant.configure("2") do |config|
     c.vm.network :forwarded_port, guest: 8000, host: 8000
 
     c.vm.synced_folder ".", "/vagrant", :nfs => true, id: "vagrant-root"
-    c.vm.provision "shell", path: "conf/install.sh"
   end
 
   config.vm.define :rds do |c|
@@ -28,24 +27,32 @@ Vagrant.configure("2") do |config|
     c.vm.box = "rds"
     c.vm.guest = :windows
 
-    c.vm.synced_folder ".", "/cygdrive/c/vagrant", type: "rsync", rsync__exclude: [".hg/", "software"]
+    # c.vm.synced_folder ".", "/cygdrive/c/vagrant", type: "rsync", rsync__exclude: [".hg/", "software"]
     # c.vm.synced_folder ".", "/vagrant", :type => "smb"
 
-    # Forward rdp
-    c.vm.network :forwarded_port, guest: 3389, host: 3389
     # Forward IIS
     c.vm.network :forwarded_port, guest: 80, host: 8001
   end
 
   config.vm.define :windows do |c|
+    # hostname doesn't work
     c.vm.hostname = "test"
     c.vm.box_url = "http://192.168.50.137/windows-2012R2.box"
     # c.vm.box_url = "file:///srv/boxes/windows-2012R2.box"
     c.vm.box = "windows-2012R2"
     c.vm.guest = :windows
-    c.vm.synced_folder ".", "S:", :nfs => true, id: "vagrant-root"
 
+    # 
+    # c.vm.synced_folder ".", "S:", :nfs => true, id: "vagrant-root"
+
+    # Works, but sub-optimal due to manual sync
     # c.vm.synced_folder ".", "/cygdrive/c/vagrant", type: "rsync", rsync__exclude: [".hg/", "software"]
+
+    # Would prefer this, but is not supported
+    # c.vm.synced_folder ".", "/vagrant", :type => "smb"
+
+    # Forward rdp
+    c.vm.network :forwarded_port, guest: 3389, host: 3389
   end
 
 end
--- conf/install.sh
+++ conf/install.sh
@@ -1,35 +1,57 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
+
+if [ "$(id -u)" != "0" ]; then
+    echo "ERROR! You must execute the script as the 'root' user."
+    exit 1
+fi
 
 # NOTE: When run with vagrant this script is present in /tmp
-rsync -avp /vagrant/conf/etc/ /etc/
+echo '==> Copying configuration to /etc'
+rsync -rlptv /vagrant/conf/etc/ /etc/
 
 mkdir -p /srv/www
 ln -s /vagrant /srv/www/rds
 
+echo '==> Installing apt packages'
 apt-get update
-apt-get --yes install python-pip samba nginx sqlite3 git ipython rabbitmq-server
+
+PACKAGES="
+git
+ipython
+nginx
+python-pip
+python-dev
+rabbitmq-server
+samba
+sqlite3 
+"
+apt-get --yes install $PACKAGES 
 
 # NOTE: We don't need this in production just the files
 # bower
-apt-get --yes install nodejs nodejs-legacy npm
+DEV_PACKAGES="
+nodejs 
+nodejs-legacy
+npm
+"
+apt-get --yes install $DEV_PACKAGES 
 
 npm install -g bower
 
-# setup django
-pip install -r /vagrant/conf/requirements.txt
-pip install django gunicorn django-crispy-forms django-bower
-
-/vagrant/manage.py syncdb --noinput
-su vagrant -c 'yes n | /vagrant/manage.py bower install'
-/vagrant/manage.py collectstatic --noinput
-
-# setup webserver
+echo '==> Installing Webserver'
 mkdir /var/run/gunicorn
 chown www-data: /var/run/gunicorn
 rm /etc/nginx/sites-enabled/default /etc/nginx/sites-available/default
 ln -s /etc/nginx/sites-available/rds /etc/nginx/sites-enabled/rds
 
-# setup samba
+echo '==> Installing Webapp'
+pip install -r /vagrant/conf/requirements.txt
+
+/vagrant/manage.py syncdb --noinput
+su vagrant -c 'yes n | /vagrant/manage.py bower install'
+/vagrant/manage.py collectstatic --noinput
+
+echo '==> Installing Samba'
 mkdir /srv/samba
 chown -R www-data: /srv/samba
 
@@ -63,21 +85,18 @@ cat >> /etc/samba/smb.conf <<EOF
     create mask = 0777
 EOF
 
-service nmbd restart
-service smbd restart
-
-# celery
+echo '==> Enable Celery Service'
 useradd celery
 update-rc.d celeryd defaults 
 update-rc.d celeryd enable
-service celeryd start
  
-# winexe
-#ln -s /home/vagrant/src/winexe/bin/winexe /usr/local/bin
-
-# start gunicorn on startup
+echo '==> Enable Gunicorn Service'
 update-rc.d gunicorn defaults
 update-rc.d gunicorn enable
 
-service gunicorn start
+echo '==> Restarting services'
+service nmbd restart
+service smbd restart
 service nginx restart
+service gunicorn restart
+service celeryd restart
