d7b425a5c448a6e03575899a53ee316ebcf62b10
Jakob A. Dam <jakob@cabo.dk>
2014-11-28 10:49:14 +0100

    get or set DNS.

--- winadm.py
+++ winadm.py
@@ -7,36 +7,75 @@ log = logging.getLogger(__name__)
 
 HERE = os.path.dirname(os.path.abspath(__file__))
 
-session = None
+def run_cmd(command, host, **kwargs):
+    return run(command, host, interpreter='cmd', **kwargs)
 
-def insert_ps_args(script, args=()):
-    for i,arg in enumerate(args):
-        old = "$args[{}]".format(i)
-        new = '"{}"'.format(arg)
-        script = script.replace(old, new)
-    return script
+def run_ps(command, host, **kwargs):
+    return run(command, host, interpreter='ps', **kwargs)
 
-def run_script(fd, args=()):
+def run_script(fd, host, **kwargs):
     ext = os.path.splitext(fd.name)[1]
-    content = fd.read()
+    command = fd.read()
 
     if "ps" in ext:
-        if args:
-            content = insert_ps_args(content, args)
-        return session.run_ps(content)
-    else:
-        return session.run_cmd(content, *args)
+        return run(command, host, interpreter='ps', **kwargs)
 
-def hostname(name=None, **kwargs):
+    return run(command, host, interpreter='ps', **kwargs)
+
+def run(command, host,
+    interpreter='cmd',
+    auth=None,
+    args=None
+    ):
+    """Runs a remote command.
+    Returns result string
+
+    :param command: command to execute
+    :param host: host to execute on
+    :param auth: (optional) Auth tuple to enable Auth.
+    :param args: (optional) Tuple of command arguments.
+    :param interpreter: (optional) Interpreter to use cmd or ps. Set to 'cmd' by default.
+
+    """
+    session = winrm.Session(host, auth=auth)
+    if interpreter == 'cmd':
+        response = session.run_cmd(command, args)
+    else:
+        response = session.run_ps(command, args)
+    return response.std_out.rstrip('\n')
+
+def hostname(host, name=None, **kwargs):
     """Get or set the system's host name
+
+    :param host: host to execute command on
+    :param name: (optional) set hostname to this
     """
     if not name:
-        return session.run_cmd('hostname')
+        return run('hostname', host, **kwargs)
     else:
-        return session.run_ps('Rename-Computer -Restart -NewName "%s"' % name)
+        ps = 'Rename-Computer -Restart -NewName "{}"'.format(name)
+        return run_ps(ps, host, **kwargs)
 
-def domain(name=None):
+def dns(host, ips=(), **kwargs):
+    """Get or set DNS for the host
+
+    :param host:
+    """
+    if not ips:
+        script = open('{}/scripts/dns-get.ps1'.format(HERE))
+        return run_script(script, host, **kwargs)
+    else:
+        script = open('{}/scripts/dns-set.ps1'.format(HERE))
+        return run_script(script, host, args=ips, **kwargs)
+    # ps = 'ls'
+    # ps = '(Get-DncClientServerAddress -InterfaceAlias "Ethernet" -AddressFamily IPv4).ServerAddresses'
+    # return run_ps(ps, host, **kwargs)
+
+def domain(host, name=None):
     """Get or set the system's domain name
+
+    :param host: host to execute command on
+    :param name: (optional) if present sets the domain to this value
     """
     if not name:
         return session.run_ps('Get-ADDomain | Select -expand DNSRoot')
@@ -44,18 +83,15 @@ def domain(name=None):
         pass
         #TODO
 
-def whereis(name):
+def whereis(host, name, **kwargs):
     script = open('{}/scripts/whereis.ps1'.format(HERE))
-    return run_script(script, args=[name])
+    return run_script(script, host, args=[name], **kwargs)
 
-def set_session(s):
-    global session
-    session = s
-    
-if __name__ == '__main__':    
-    ip = '192.168.123.71'
-    user = 'Vagrant'
+if __name__ == '__main__':
+    ip = '192.168.123.72'
+    user = 'vagrant'
+    password = 'V@grant'
     # set_session(winrm.Session(ip, auth=(user, password), transport='kerberos'))
-    set_session(winrm.Session(ip, auth=(user, password)))
-    res = whereis('')
-    print res.std_out
+    # print dns(ip, ips=('8.8.8.8','8.8.4.4'), auth=(user,password))
+    print dns(ip, auth=(user,password))    
+    # print whereis(ip, '*', auth=(user,password))    
