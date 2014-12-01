90091befd25357134617dd6f9f62cf23992b1f71
Jakob A. Dam <jakob@cabo.dk>
2014-12-01 08:42:33 +0100

    Fixed script path in tests.

--- rds/tests/test_models.py
+++ rds/tests/test_models.py
@@ -12,18 +12,18 @@ from rds import models
 
 test_settings = {
     'PACKAGE_DIR': settings.TEST_PACKAGE_DIR,
-    'MEDIA_ROOT': settings.TEST_PACKAGE_DIR 
+    'MEDIA_ROOT': settings.TEST_PACKAGE_DIR
     }
 
-def _add_file(package):    
-    settings.SOFTWARE_ROOT = settings.BASE_DIR + '/software' 
-    
+def _add_file(package):
+    settings.SOFTWARE_ROOT = settings.BASE_DIR + '/software'
+
     filename = 'Firefox Setup 31.0.exe'
 
     path = os.path.join(settings.SOFTWARE_ROOT, filename)
     test_path = models.generate_filename(package, filename)
     assert os.path.isfile(path), "There is no file there"
-    
+
     dirname = os.path.dirname(test_path)
     if not os.path.exists(dirname):
         os.makedirs(dirname)
@@ -38,7 +38,7 @@ class TestPackage(TestCase):
     def setUp(self):
         self.name = 'Firefox'
         self.version = '31.0'
-        self.args = '-ms'        
+        self.args = '-ms'
 
         self.p = Package(name=self.name, args=self.args, version=self.version)
 
@@ -70,22 +70,22 @@ class TestPackage(TestCase):
         expected = settings.PACKAGE_DIR + '/firefox_31.0/software/Firefox Setup 31.0.exe'
         self.assertEqual(path, expected)
 
-    def test_script_path(self):        
+    def test_script_path(self):
         self.p.save()
         self.p.installer = self.p.find_installer()
         path = self.p.test_script_path
-        expected = settings.PACKAGE_DIR + '/firefox_31.0/script/firefox_install.bat'
+        expected = settings.PACKAGE_DIR + '/firefox_31.0/script/firefox_31.0_install.bat'
         self.assertEqual(path, expected)
 
     def test_log_path(self):
         expected = settings.PACKAGE_DIR + '/firefox_31.0/log/firefox_31.0.log'
         self.assertEqual(self.p.log_path, expected)
-                
+
     def test_add_dirs(self):
         self.p.add_dirs()
         path = settings.PACKAGE_DIR + '/firefox_31.0/script'
         self.assertTrue(os.path.exists(path))
-                
+
     def test_samba_path(self):
         samba_path = self.p.samba_path
         expected = r'\\ubuntu\share\firefox_31.0'
@@ -95,11 +95,11 @@ class TestPackage(TestCase):
         _add_file(self.p)
         self.p.save()
         self.p.installer = self.p.find_installer()
-        
+
         samba_path = self.p.samba_path_installer
         expected = r'\\ubuntu\share\firefox_31.0\software\Firefox Setup 31.0.exe'
         self.assertEqual(samba_path, expected)
-        
+
     def test_install_cmd(self):
         _add_file(self.p)
         self.p.save()
@@ -108,5 +108,3 @@ class TestPackage(TestCase):
         expected = '"%s" %s' % (r"\\ubuntu\share\firefox_31.0\software\Firefox Setup 31.0.exe", self.args)
         actual = self.p.install_cmd
         self.assertEqual(actual, expected)
-
-        
