3b5e79745e99724816a087278975e1c8bf4a7a2a
Jakob A. Dam <jakob@cabo.dk>
2015-01-27 16:09:00 +0100

    Upload progress bar.

--- rds/static/rds/js/fileprogress.js
+++ rds/static/rds/js/fileprogress.js
@@ -7,7 +7,15 @@
 (function($){
     "use strict";
 
-     var template = '<div class="modal fade" id="file-progress-modal">\
+    $.support.xhrFileUpload = !!(window.FileReader && window.ProgressEvent);
+    $.support.xhrFormData = !!window.FormData;
+
+    if(!$.support.xhrFileUpload || !$.support.xhrFormData){
+        // skip decorating form
+        return;
+    }
+
+    var template = '<div class="modal fade" id="file-progress-modal">\
   <div class="modal-dialog">\
     <div class="modal-content">\
       <div class="modal-header">\
@@ -52,7 +60,6 @@
         },
 
         reset: function(){
-            console.log('reset');
             this.$modal_title = $('.modal-title').text('Uploading');
             this.$modal_footer.hide();
             this.$modal_bar.addClass('progress-bar-success');
@@ -68,51 +75,55 @@
                 keyboard: false
             });
 
+
+            // We need the native XMLHttpRequest for the progress event
+            var xhr = new XMLHttpRequest();
+            xhr.open(form.attr('method'), window.location.href);
+            xhr.setRequestHeader('X-REQUESTED-WITH', 'XMLHttpRequest');
+
+            xhr.onload = $.proxy(this.success, this, xhr);
+            xhr.onerror = $.proxy(this.error, this, xhr);
+            xhr.upload.onprogress = $.proxy(this.progress, this);
             var data = new FormData(form.get(0));
-            var xhr = $.ajax(window.location.href, {
-                data: data,
-                contentType: false,
-                processData: false,
-                success: $.proxy(this.success, this),
-                error: $.proxy(this.error, this),
-                type: form.attr('method')
-            });
-
-            // var xhr = new XMLHttpRequest();
-            // xhr.open(form.attr('method'), window.location.href);
-            // xhr.send(data);
-
-            $.proxy(xhr.progress, this.progress);
+            xhr.send(data);
         },
 
-        success: function(data, status, xhr) {
-            debugger;
+        success: function(xhr) {
+            var response = $.parseJSON(xhr.responseText);
+            var url = response.location;
+            this.set_progress(100);
+            window.location.href = url;
         },
 
-        error: function(xhr, status, error){
+        error: function(xhr){
             this.$modal_title.text('Upload failed');
 
             this.$modal_bar.removeClass('progress-bar-success');
             this.$modal_bar.addClass('progress-bar-danger');
             this.$modal_footer.show();
 
-            var h = $.parseHTML(xhr.responseText);
-            var new_form = $(h).find('form');
+            if(xhr.getResponseHeader('Content-Type') === 'application/json'){
+                // maybe do something
+            }
+            else{
+                // update form contents
+                var h = $.parseHTML(xhr.responseText);
+                var new_form = $(h).find('form');
+                new_form.find(':file').filestyle({buttonBefore: true});
+                this.$form.html(new_form.children());
+            }
+        },
 
-            //this.constructor();
-
-            new_form.find(':file').filestyle({buttonBefore: true});
-            this.$form.html(new_form.children());
-            //this.$form.html($('<p>wii</p>'));
+        set_progress: function(percent){
+            this.$modal_bar.attr('aria-valuenow', percent);
+            this.$modal_bar.text(percent + '%');
+            this.$modal_bar.css('width', percent + '%');
         },
 
         progress: function(/*ProgressEvent*/e){
             var percent = Math.round((e.loaded / e.total) * 100);
-            this.$modal_bar.attr('aria-valuenow', percent);
-            this.$modal_bar.text(percent + '%');
-            this.$modal_bar.css('width', percent + '%');
+            this.set_progress(percent);
         }
-
     };
 
     $.fn.fileprogress = function(options, value){
@@ -127,10 +138,3 @@
     };
 
 })(window.jQuery);
-
-//   if(!Modernizr.filereader){
-//     // skip decorating form
-//     return;
-//   }
-
-// });
