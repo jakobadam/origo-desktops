3d3e6d14799756b802bbf22e7973c6af76845fe0
Jakob A. Dam <jakob@cabo.dk>
2015-05-20 13:34:11 +0200

    RDS scripts moved to scripts/rds

--- scripts/rds/add-deployment.ps1
+++ scripts/rds/add-deployment.ps1
@@ -1,13 +1,18 @@
+# Single Server RDS deployment, i.e. the server contains all components of RDS
+# Remember to run as Domain Administrator
 Import-Module RemoteDesktop
 
-#$fqdn = [System.Net.Dns]::GetHostByName($env:COMPUTERNAME).HostName
-$fqdn = 'rds-new.example.com'
+
+
+# Can't be run from the server itself. Because it can't restart itself?!?
+# $fqdn = [System.Net.Dns]::GetHostByName($env:COMPUTERNAME).HostName
+$fqdn = 'rds.example.com'
 
 New-RDSessionDeployment -ConnectionBroker $fqdn `
     -WebAccessServer $fqdn `
     -SessionHost $fqdn
 
-New-RDSessionCollection -CollectionName 'My collection' `
+New-RDSessionCollection -CollectionName 'App Collection' `
    -SessionHost $fqdn `
    -CollectionDescription 'Desc' `
    -ConnectionBroker $fqdn 
