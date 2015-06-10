7430a0137838aef58ec288d3e495ac02f23573eb
Jakob A. Dam <jakob.a.dam@gmail.com>
2015-06-10 10:36:11 +0200

    Remove pass

--- runserver.sh
+++ runserver.sh
@@ -1,2 +1,2 @@
 # Run the dev server under www-data, so upload works.
-echo 'vagrant' | sudo -S -u www-data `readlink -f manage.py` runserver 0.0.0.0:8000
+sudo -S -u www-data `readlink -f manage.py` runserver 0.0.0.0:8000
